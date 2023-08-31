7a8f79fcc55161cf9b60984b52e6db9f49af4294
wataru.endo <endo0430w@gmail.com>
2023-09-01 00:36:00 +0900

    Add Header components

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <div className="flex flex-row h-20 border-b border-solid border-gray-600">
+      <div className="my-auto">
+        <p className="font-sans text-bold text-white">gluttony</p>
+      </div>
+    </div>
+  )
+}
